d1ce8c51cd5f51bd54c1ec63fbae9efd7db86def
Andrii Cherepakha <achere@softserveinc.com>
2019-06-21 18:02:42 +0300

    15 3 Sum

---
+++ LeetCode/Integers/15_SumOfTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode.Integers
+{
+    public class SumOfTree
+    {
+        [Test]
+        public void Test1()
+        {
+            var nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            var list = ThreeSum(nums);
+        }
+
+
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            Array.Sort(nums); // n log(n)
+            
+
+            return null;
+        }
+    }
+}
