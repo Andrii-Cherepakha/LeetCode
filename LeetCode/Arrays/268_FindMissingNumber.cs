28804253ca81faea63146465316dd732286bdddb
Andrii Cherepakha <achere@softserveinc.com>
2019-06-25 18:42:44 +0300

    268 FindMissingNumber

---
+++ LeetCode/Arrays/268_FindMissingNumber.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCode.Arrays
+{
+    public class FindMissingNumber
+    {
+        public int MissingNumber(int[] nums)
+        {
+            long expectedSum = nums.Length * (nums.Length + 1) / 2;
+
+            long actualSum = 0;
+            foreach (var num in nums)
+            {
+                actualSum = actualSum + num;
+            }
+
+            return (int)(expectedSum - actualSum);
+        }
+    }
+}
