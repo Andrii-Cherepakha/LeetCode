1b9660fd43498739a4f8fc2ad8e29e714f86a263
Andrii Cherepakha <achere@softserveinc.com>
2019-07-09 15:35:06 +0300

    BinaryHeapMax

---
+++ LeetCode/DataStructure/BinaryHeapMax.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace LeetCode.DataStructure
+{
+    public class BinaryHeapMax
+    {
+        private int[] _arr;
+        private int _tail = -1;
+
+        public BinaryHeapMax(int size)
+        {
+            _arr = new int[size];
+        }
+        
+        public bool IsEmpty => _tail == -1;
+
+        public int[] Array => _arr;
+
+        // log(N)
+        public void Push(int value)
+        {
+            if (++_tail >= _arr.Length)
+            {
+                throw new IndexOutOfRangeException($"Cannot insert value. Array capacity: {_arr.Length} Index: {_tail}");
+            }
+
+            // put new value at the end
+            _arr[_tail] = value;
+
+            // walk up the tree and swap current value with parent if current value is greater than parent
+            int currentIndex = _tail;
+            int parentIndex = GetParent(_tail);
+            while (parentIndex >= 0 && _arr[currentIndex] > _arr[parentIndex])
+            {
+                // swap values
+                _arr[currentIndex] = _arr[parentIndex];
+                _arr[parentIndex] = value;
+                // update indexes
+                currentIndex = parentIndex;
+                parentIndex = GetParent(currentIndex);
+            }
+        }
+
+        public int Pop()
+        {
+            if (_tail == -1)
+            {
+                throw new IndexOutOfRangeException("Heap is empty");
+            }
+
+            int top = _arr[0];
+
+            _arr[0] = _arr[_tail];
+            _arr[_tail] = 0; // clean up the value for debug purpose
+            _tail--;
+
+            int currentIndex = 0;
+            while (LeftLeafExists(currentIndex))
+            {
+                int biggestValueIndex = GetLeftLeafIndex(currentIndex);
+
+                if (RightLeafExists(currentIndex) &&
+                    _arr[GetRightLeafIndex(currentIndex)] > _arr[GetLeftLeafIndex(currentIndex)])
+                {
+                    biggestValueIndex = GetRightLeafIndex(currentIndex);
+                }
+
+                if (_arr[currentIndex] >= _arr[biggestValueIndex])
+                {
+                    break;
+                }
+                
+                // swap
+                int tmp = _arr[currentIndex];
+                _arr[currentIndex] = _arr[biggestValueIndex];
+                _arr[biggestValueIndex] = tmp;
+                // walk down
+                currentIndex = biggestValueIndex;
+            }
+
+            return top;
+        }
+
+        private int GetParent(int i) { return (i - 1) / 2; }
+
+        private int GetLeftLeafIndex(int i) { return i * 2 + 1; }
+        private int GetRightLeafIndex(int i) { return i * 2 + 2; }
+
+        private bool LeftLeafExists(int i) { return GetLeftLeafIndex(i) <= _tail; }
+        private bool RightLeafExists(int i) { return GetRightLeafIndex(i) <= _tail; }
+    }
+}
--- LeetCode/Tests/PriorityQueueTests.cs
+++ LeetCode/Tests/PriorityQueueTests.cs
@@ -10,7 +10,7 @@ namespace LeetCode.Tests
         [Test]
         public void PriorityQueuePush1()
         {
-            var queue = new PriorityQueue(7);
+            var queue = new BinaryHeapMax(7); // PriorityQueue
             queue.Push(1);
             queue.Push(2);
             queue.Push(3);
@@ -25,7 +25,7 @@ namespace LeetCode.Tests
         [Test]
         public void PriorityQueuePush2()
         {
-            var queue = new PriorityQueue(7);
+            var queue = new BinaryHeapMax(7); // PriorityQueue
             queue.Push(7);
             queue.Push(6);
             queue.Push(5);
@@ -40,7 +40,7 @@ namespace LeetCode.Tests
         [Test]
         public void PriorityQueuePop1()
         {
-            var queue = new PriorityQueue(7);
+            var queue = new BinaryHeapMax(7);  // PriorityQueue
             queue.Push(1);
             queue.Push(2);
             queue.Push(3);
@@ -61,7 +61,7 @@ namespace LeetCode.Tests
         [Test]
         public void PriorityQueuePop2()
         {
-            var queue = new PriorityQueue(7);
+            var queue = new BinaryHeapMax(7);  // PriorityQueue
             queue.Push(7);
             queue.Push(6);
             queue.Push(5);
@@ -69,6 +69,52 @@ namespace LeetCode.Tests
             queue.Push(3);
             queue.Push(2);
             queue.Push(1);
+
+            // pop
+            while (!queue.IsEmpty)
+            {
+                int top = queue.Pop();
+                var arr = queue.Array;
+                Console.WriteLine(top);
+            }
+        }
+
+        [Test]
+        public void PriorityQueuePop3()
+        {
+            var queue = new BinaryHeapMax(9);  // PriorityQueue
+            queue.Push(1);
+            queue.Push(12);
+            queue.Push(3);
+            queue.Push(15);
+            queue.Push(5);
+            queue.Push(17);
+            queue.Push(7);
+            queue.Push(19);
+            queue.Push(9);
+
+            // pop
+            while (!queue.IsEmpty)
+            {
+                int top = queue.Pop();
+                var arr = queue.Array;
+                Console.WriteLine(top);
+            }
+        }
+
+        [Test]
+        public void PriorityQueuePop4()
+        {
+            var queue = new BinaryHeapMax(9);  // PriorityQueue
+            queue.Push(1);
+            queue.Push(2);
+            queue.Push(3);
+            queue.Push(4);
+            queue.Push(5);
+            queue.Push(100);
+            queue.Push(50);
+            queue.Push(40);
+            queue.Push(20);
 
             // pop
             while (!queue.IsEmpty)
