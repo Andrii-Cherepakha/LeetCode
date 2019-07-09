b2e2c2930c0208648f1df8f6a36b4ae37e73ad91
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-07-10 02:54:41 +0300

    18 SumOfFour

--- LeetCode/Integers/18_SumOfFour.cs
+++ LeetCode/Integers/18_SumOfFour.cs
@@ -1,13 +1,107 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Integers
 {
     public class SumOfFour
     {
 
+        [Test]
+        public void Test()
+        {
+            int[] nums = new int[] { 1, 0, -1, 0, -2, 2 };
+            int target = 0;
+            FourSum(nums, target);
+        }
 
-        public IList<IList<int>> FourSum(int[] nums, int target)
+        [Test]
+        public void Test2()
+        {
+            int[] nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            int target = -1;
+            FourSum(nums, target);
+        }
+
+
+        public IList<IList<int>> FourSum2(int[] nums, int target) // N*N ? ACCEPTED
+        {
+            IList<IList<int>> list = new List<IList<int>>();
+
+            if (nums == null)
+            {
+                return list;
+            }
+
+            var dict = new Dictionary<int, List<int[]>>();
+
+            for (int i = 0; i < nums.Length - 1; i++) // N*N
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var key = nums[i] + nums[j]; // O(N) ?
+                    if (!dict.ContainsKey(key))
+                    {
+                        dict[key] = new List<int[]>();
+                    }
+
+                    bool exists = false;
+                    foreach (int[] arr in dict[key])
+                    {
+                        if ((arr[0] == nums[i] && arr[1] == nums[j]) ||
+                            (arr[1] == nums[i] && arr[0] == nums[j]))
+                        {
+                            exists = true;
+                            break;
+                        }
+                    }
+
+                    if (!exists)
+                    {
+                        dict[key].Add(new[] { i, j }); // add indexes not values
+                    }
+                }
+            }
+
+            foreach (int key in dict.Keys) // O(N) ?
+            {
+                int key2 = target - key;
+
+                if (!dict.ContainsKey(key2)) continue;
+
+                foreach (int[] arr1 in dict[key]) // for each element in first list 
+                {
+                    foreach (int[] arr2 in dict[key2]) // for each element in second list 
+                    {
+                        if (arr1.Union(arr2).ToArray<int>().Distinct().Count() < 4) continue; // remove duplicates indexes
+
+                        var item = new List<int>() { nums[arr1[0]], nums[arr1[1]], nums[arr2[0]], nums[arr2[1]] };
+                        item.Sort();
+
+                        bool exists = false;
+                        foreach (var arr in list)
+                        {
+                            if (arr[0] == item[0] && arr[1] == item[1] && arr[2] == item[2] && arr[3] == item[3])
+                            {
+                                exists = true;
+                                break;
+                            }
+                        }
+
+                        if (!exists)
+                        {
+                            list.Add(item);
+                        }
+                    }
+                }
+            }
+
+            return list;
+        }
+
+         
+        public IList<IList<int>> FourSum(int[] nums, int target) // N^3 ACCEPTED
         {
             IList<IList<int>> list = new List<IList<int>>();
 
@@ -29,7 +123,7 @@ namespace LeetCode.Integers
                 for (int b = a + 1; b < nums.Length - 2; b++)
                 {
                     // skip duplicates
-                    if (b > 1 && nums[b] == nums[b - 1])
+                    if (b > a + 1 && nums[b] == nums[b - 1])
                     {
                         continue;
                     }
@@ -63,7 +157,7 @@ namespace LeetCode.Integers
                     }
                 }
             }
-            
+
             return list;
         }
     }
