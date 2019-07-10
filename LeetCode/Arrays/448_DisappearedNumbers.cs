9d5f5870ea359b155d789e6353c155101d086f94
Andrii Cherepakha <achere@softserveinc.com>
2019-07-10 18:05:50 +0300

    448 DisappearedNumbers

--- LeetCode/Arrays/448_DisappearedNumbers.cs
+++ LeetCode/Arrays/448_DisappearedNumbers.cs
@@ -17,6 +17,34 @@ namespace LeetCode.Arrays
             }
         }
 
+        public IList<int> FindDisappearedNumbersLeetCode(int[] nums)
+        {
+            if (nums == null) return null;
+
+            var list = new List<int>();
+
+            // iterate through the input array and mark elements as negative 
+            // In this way all the numbers that we have seen will be marked as negative
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int index = Math.Abs(nums[i]) - 1; // it can be already marked as negative on previous step
+                if (nums[index] > 0) // can be already marked as negative. do not do it again
+                {
+                    nums[index] = -nums[index];
+                }
+            }
+
+            // In the second iteration, if a value is not marked as negative,
+            // it implies we have never seen that index before, so just add it to the return list.
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] > 0)
+                    list.Add(i+1);
+            }
+
+            return list;
+        }
+
         public IList<int> FindDisappearedNumbers(int[] nums)
         {
             if (nums == null) return null;
