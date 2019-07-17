18f5be9515ea400f75e93f9b0177ecd659016a9e
Andrii Cherepakha <achere@softserveinc.com>
2019-07-17 18:32:21 +0300

    167 TwoSum2Ordered

---
+++ LeetCode/Arrays/167_TwoSum2Ordered.cs
@@ -0,0 +1,31 @@
+﻿
+namespace LeetCode.Arrays
+{
+    public class TwoSum2Ordered
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int start = 0;
+            int end = numbers.Length - 1;
+
+            while (start < end)
+            {
+                int sum = numbers[start] + numbers[end];
+                if (sum == target)
+                {
+                    return new int[] { start + 1, end + 1 };
+                }
+                else if (sum > target)
+                {
+                    end--;
+                }
+                else if (sum < target)
+                {
+                    start++;
+                }
+            }
+
+            return new int[] { 0, 0};
+        }
+    }
+}
