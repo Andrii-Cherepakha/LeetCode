3e37a6e12b37bffa34dbf63e28bac5a8f4fb1f2a
Andrii Cherepakha <achere@softserveinc.com>
2019-07-18 18:50:12 +0300

    189 RotateArray

--- LeetCode/Arrays/189_RotateArray.cs
+++ LeetCode/Arrays/189_RotateArray.cs
@@ -134,6 +134,36 @@ namespace LeetCode.Arrays
             ArrayHelper.PrintArray(nums);
         }
 
+        [Test]
+        public void Test7()
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int k = 4;
+            ArrayHelper.PrintArray(nums);
+            Rotate(nums, k);
+            ArrayHelper.PrintArray(nums);
+        }
+
+        [Test]
+        public void Test8()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6 };
+            int k = 4;
+            ArrayHelper.PrintArray(nums);
+            Rotate(nums, k);
+            ArrayHelper.PrintArray(nums);
+        }
+
+        [Test]
+        public void Test9()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+            int k = 4;
+            ArrayHelper.PrintArray(nums);
+            Rotate(nums, k);
+            ArrayHelper.PrintArray(nums);
+        }
+
         public void Rotate(int[] nums, int k)
         {
             if (nums == null || nums.Length == 0)
@@ -151,8 +181,23 @@ namespace LeetCode.Arrays
                 return;
             }
 
-            int storage = 0;
-            int i = k;
+            int storage = 0; // storage means first k elements
+            int i = k; // first position for rotation
+
+            // 1 2 3 ... k 10 11 12 .... N
+            // 1) put k    element into 0 position
+            //    put k+1  element into 1 position
+            //    put k+2  element into 2 position
+            // and so on till we meat k position
+            // once k position is reached, reset counter to 0
+            // repeat till the ned of the array
+            // Example:
+            // s       i
+            // 1 2 3 4 5 6 7 8 9 10 11 12   k = 4
+            // s               i
+            // 5 6 7 8 1 2 3 4 9 10 11 12  - once k position reached first time
+            // s                          i
+            // 9 10 11 12 1 2 3 4 5 6 7 8  - once k position reached second time
 
             while (i < nums.Length) // O(N)
             {
@@ -169,27 +214,45 @@ namespace LeetCode.Arrays
                 storage++;
             }
 
+            // if k multiple array length no need to do any additional steps 
             if (nums.Length % k == 0)
             {
                 return; // no need to deal with the reminder [0 .. k]
             }
 
             // remainder [0 .. k] is not sorted as needed
-            i = 0;
-            while (storage < k)  // O(k)
+            // Otherwise there can be several cases:
+            // Example: k = 4
+            // case 1            case 2            case 3
+            // 1 2 3 4 5         1 2 3 4 5 6       1 2 3 4 5 6 7
+            // after previous loop:
+            //   s                   s                   s
+            // 5 2 3 4 1         5 6 3 4 1 2       5 6 7 4 1 2 3
+            // we need to move all elements starting from s position to the left up to 0 position
+            // each time an element is moved, increase position
+            // 5 2 3 4           5 6 3 4           5 6 7 4 
+            // 2 5                 3 6                 4 7 
+            //   3 5             3 5                 4 6
+            //     4 5               4 6           4 5
+            //                     4 5             
+            // 2 3 4 5           3 4 5 6           4 5 6 7
+
+
+            int position = 0;
+            while (storage < k) // O(k)
             {
-                int tmp = nums[storage];
-                nums[storage] = nums[i];
-                nums[i] = tmp;
+                i = storage; // start with s
+                while (i - 1 >= position) // while the left is not reached
+                {
+                    int tmp = nums[i];  // swap
+                    nums[i] = nums[i - 1];
+                    nums[i - 1] = tmp;
+                    i--;  // and move to the left
+                }
 
-                i++;
-                storage++;
-                //if (storage == k)
-                //{
-                //    storage = k - 1;
-                //}                
+                storage++;  // go to the next element
+                position++; // that will be placed in left + 1 position
             }
-
         }
     }
 }
---
+++ LeetCode/Arrays/189_RotateArrayLeetCode.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode.Arrays
+{
+    public class RotateArrayLeetCode
+    {
+
+
+        public void Rotate(int[] nums, int k)
+        {
+            // Idea: reverse initial array, then reverse each part
+            k %= nums.Length; // if k > nums.Length
+
+            // Original List:                             1 2 3 | 4 5 6 7
+            // 1. Revers initial array:                   7 6 5 | 4 3 2 1
+            Reverse(nums, 0, nums.Length - 1);
+            // 2. Revers first k numbers                  5 6 7 | 4 3 2 1
+            Reverse(nums, 0, k - 1);
+            // 3. Revers last n - k numbers               5 6 7 | 1 2 3 4
+            Reverse(nums, k, nums.Length - 1);
+        }
+
+        public void Reverse(int[] nums, int start, int end)
+        {
+            while (start < end)
+            {
+                int tmp = nums[end];
+                nums[end] = nums[start];
+                nums[start] = tmp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
