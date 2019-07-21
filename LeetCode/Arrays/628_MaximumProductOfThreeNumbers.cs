4d5009184ca59b8ee4c47d70d6c3fa8f0d2e641e
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-07-21 18:07:34 +0300

    628 MaximumProductOfThreeNumbers

---
+++ LeetCode/Arrays/628_MaximumProductOfThreeNumbers.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace LeetCode.Arrays
+{
+    class MaximumProductOfThreeNumbers
+    {
+        [Test]
+        public void Example2()
+        {
+            int[] nums = { 1, 2, 3, 4 };
+            int expected = 24;
+            Assert.That(MaximumProduct(nums), Is.EqualTo(expected));
+        }
+               
+        public int MaximumProduct(int[] nums) // N log(N) ACCEPTED
+        {
+            Array.Sort(nums);
+
+            int max = nums[nums.Length - 1] * nums[nums.Length - 2] * nums[nums.Length - 3];
+            int min = nums[nums.Length - 1] * nums[0] * nums[1];
+
+            return Math.Max(max, min);
+        }
+    }
+}
