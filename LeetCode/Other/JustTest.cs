dc581f82e302a908a45b5530f4368688c91530b8
Andrii Cherepakha <achere@softserveinc.com>
2019-07-26 13:26:15 +0300

    ContainsSubStringTest

--- LeetCode/Other/JustTest.cs
+++ LeetCode/Other/JustTest.cs
@@ -14,5 +14,27 @@ namespace LeetCode
                 Console.WriteLine($"1 << {i} == 2^{i} == {result}");
             }
         }
+
+        [Test]
+        public void ContainsSubStringTest()
+        {
+            int c = ContainsSubString("AAAAA", "A");
+        }
+
+        private int ContainsSubString(string word, string sub)
+        {
+            int counter = 0;
+
+            int pos = word.IndexOf(sub, StringComparison.Ordinal);
+
+            while (pos >= 0)
+            {
+                counter++;
+                word = word.Substring(pos + 1, word.Length - 1 - pos);
+                pos = word.IndexOf(sub, StringComparison.Ordinal);
+            }
+            
+            return counter;
+        }
     }
 }
