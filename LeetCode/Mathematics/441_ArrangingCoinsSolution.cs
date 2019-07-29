798e148e1f3d011095b69ea04dd05b3247244bdc
Andrii Cherepakha <achere@softserveinc.com>
2019-07-29 16:10:48 +0300

    441 ArrangingCoins

---
+++ LeetCode/Mathematics/441_ArrangingCoinsSolution.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Mathematics
+{
+    public class ArrangingCoinsSolution
+    {
+        [Test]
+        public void Test1()
+        {
+            int n = 1804289383;
+            int expected = 60070;
+            Assert.That(ArrangeCoins(n), Is.EqualTo(expected));
+        }
+
+        public int ArrangeCoins(int n)
+        {
+            return (int)(-1 + System.Math.Sqrt(1 + 8 * (double) n)) / 2;
+        }
+    }
+}
