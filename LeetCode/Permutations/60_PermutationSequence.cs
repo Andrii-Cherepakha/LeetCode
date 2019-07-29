940a4742a6ade8b2e343c9d7c17e0fa3a4938575
Andrii Cherepakha <achere@softserveinc.com>
2019-07-29 15:10:38 +0300

    60 PermutationSequence my solution

--- LeetCode/Permutations/60_PermutationSequence.cs
+++ LeetCode/Permutations/60_PermutationSequence.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace LeetCode.Permutations
 {
@@ -24,11 +25,101 @@ namespace LeetCode.Permutations
         }
 
         [Test]
+        public void Example4_13()
+        {
+            int n = 4;
+            int k = 13;
+            string expected = "3124";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
         public void Example4_14()
         {
             int n = 4;
             int k = 14;
             string expected = "3142";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_1()
+        {
+            int n = 4;
+            int k = 1;
+            string expected = "1234";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_2()
+        {
+            int n = 4;
+            int k = 2;
+            string expected = "1243";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_3()
+        {
+            int n = 4;
+            int k = 3;
+            string expected = "1324";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_4()
+        {
+            int n = 4;
+            int k = 4;
+            string expected = "1342";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_5()
+        {
+            int n = 4;
+            int k = 5;
+            string expected = "1423";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_6()
+        {
+            int n = 4;
+            int k = 6;
+            string expected = "1432";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_7()
+        {
+            int n = 4;
+            int k = 7;
+            string expected = "2134";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_19()
+        {
+            int n = 4;
+            int k = 19;
+            string expected = "4123";
+            Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void Example4_24()
+        {
+            int n = 4;
+            int k = 24;
+            string expected = "4321";
             Assert.That(GetPermutation(n, k), Is.EqualTo(expected));
         }
 
@@ -78,6 +169,36 @@ namespace LeetCode.Permutations
         }
 
         public string GetPermutation(int n, int k)
+        {
+            // The set [1,2,3,...,n] contains a total of n! unique permutations.
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= n; i++) numbers.Add(i);
+
+            string result = "";
+            int position = k - 1;
+            int factorial = Fact(n - 1);
+            int factor = n - 1;
+
+            while (position > 0) // Optimization: If position == 0 no need to proceed, all further characters are in the appropriate order 
+            {
+                int range = position / factorial + 1; // range == index of the character
+                result += numbers.ElementAt(range - 1);  // - 1 due to zero indexes
+                numbers.RemoveAt(range - 1);  // - 1 due to zero indexes
+                position = position % factorial + 1 - 1; // next position
+
+                factorial /= factor;
+                factor--;
+            }
+
+            foreach (var number in numbers)
+            {
+                result += number;
+            }
+
+            return result;
+        }
+
+        public string GetPermutationLeetCode(int n, int k)
         {
             List<int> numbers = new List<int>();
             for (int i = 1; i <= n; i++) numbers.Add(i);
