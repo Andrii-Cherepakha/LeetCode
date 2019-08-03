213a097fa082faf794af15e292f40c990405954e
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-08-03 23:45:59 +0300

    206 ReverseList

---
+++ LeetCode/LinkedList/206_ReverseLinkedList.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.LinkedList
+{
+    class ReverseLinkedList
+    {
+
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode prev = null;
+            ListNode current = head;
+            ListNode next = null;
+
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+
+            return prev;
+        }
+    }
+}
