3bd4aeb5130cb98e27ebd11287f40170506f124e
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-08-05 23:14:12 +0300

    209 MinimumSizeSubarraySum

--- LeetCode/Arrays/209_MinimumSizeSubarraySum.cs
+++ LeetCode/Arrays/209_MinimumSizeSubarraySum.cs
@@ -4,8 +4,30 @@ namespace LeetCode.Arrays
 {
     class MinimumSizeSubarraySum
     {
-
         public int MinSubArrayLen(int s, int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int left = 0;
+            int sum = 0;
+            int minLength = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum = sum + nums[i];
+                while (sum >= s)
+                {
+                    minLength = Math.Min(minLength, i + 1 - left);
+                    sum = sum - nums[left];
+                    left++;
+                }
+            }
+
+            return minLength == int.MaxValue ? 0 : minLength;
+        }
+
+
+        public int MinSubArrayLen1(int s, int[] nums)
         {
             if (nums == null || nums.Length == 0) return 0;
 
@@ -31,9 +53,9 @@ namespace LeetCode.Arrays
                     sum = sum - nums[left];
                     left++;
                     length--;
-                }                
+                }
             }
-                       
+
             return minLength;
         }
     }
