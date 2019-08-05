770d22da5c17ac6eede0069df3c287ce95ed1b25
Andrii Cherepakha <achere@softserveinc.com>
2019-08-05 18:12:02 +0300

    240 SearchMatrix2

---
+++ LeetCode/Array2D/240_SearchMatrix2.cs
@@ -0,0 +1,69 @@
+﻿namespace LeetCode.Array2D
+{
+    public class SearchMatrix2
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            if (matrix == null)
+            {
+                return false;
+            }
+
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            int r = 0;
+            int c = m - 1;
+
+            while (r <= n - 1 && c >= 0)
+            {
+                if (matrix[r, c] > target)
+                {
+                    c--;
+                }
+                else if (matrix[r, c] < target)
+                {
+                    r++;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool SearchMatrix1(int[,] matrix, int target)
+        {
+            if (matrix == null)
+            {
+                return false;
+            }
+
+            int n = matrix.GetLength(0);
+            int m = matrix.GetLength(1);
+
+            int r = 0;
+            int c = m - 1;
+
+            while (r <= n - 1 && c >= 0)
+            {
+                if (matrix[r, c] == target)
+                {
+                    return true;
+                }
+                else if (matrix[r, c] > target)
+                {
+                    c--;
+                }
+                else // < target
+                {
+                    r++;
+                }
+            }
+
+            return false;
+        }
+    }
+}
