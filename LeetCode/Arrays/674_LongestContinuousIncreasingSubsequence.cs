d73156fd29700b88194a4c7be80ba8e2dfe996b8
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-08-14 22:43:46 +0300

    674 LongestContinuousIncreasingSubsequence

---
+++ LeetCode/Arrays/674_LongestContinuousIncreasingSubsequence.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Arrays
+{
+    class LongestContinuousIncreasingSubsequence
+    {
+
+        public int FindLengthOfLCIS(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int maxLength = 1;
+            int length = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    length++;
+                }
+                else
+                {
+                    maxLength = Math.Max(maxLength, length);
+                    length = 1;
+                }
+            }
+
+            return Math.Max(maxLength, length);
+        }
+    }
+}
