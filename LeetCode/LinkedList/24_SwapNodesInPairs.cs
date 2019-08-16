f4780c20ec2446d8f02b4a348142df33a86af1a7
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-08-17 01:09:16 +0300

    24 SwapNodesInPairs

---
+++ LeetCode/LinkedList/24_SwapNodesInPairs.cs
@@ -0,0 +1,28 @@
+﻿
+namespace LeetCode.LinkedList
+{
+    class SwapNodesInPairs
+    {
+
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode current = head;
+            head = head.next;
+            ListNode previous = null;
+
+            while (current != null && current.next != null)
+            {
+                ListNode tmp = current.next.next;
+                current.next.next = current;
+                if (previous != null) previous.next = current.next;
+                current.next = tmp;
+                previous = current;
+                current = tmp;
+            }
+
+            return head;
+        }
+    }
+}
