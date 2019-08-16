e5bb63c3d50f8b60ab39c56b4a716512334657e8
Andrii Cherepakha <andrey.cherepakha@gmail.com>
2019-08-16 22:02:58 +0300

    876 MiddleOfTheLinkedList

---
+++ LeetCode/LinkedList/876_MiddleOfTheLinkedList.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.LinkedList
+{
+    class MiddleOfTheLinkedList
+    {
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            var fake = new ListNode(-1);
+            fake.next = head;
+
+            ListNode turtle = fake;
+            ListNode hare = fake;
+
+            while (hare != null)
+            {
+                turtle = turtle.next;
+                hare = hare.next;
+                if (hare != null)
+                {
+                    hare = hare.next;
+                }
+            }
+
+            return turtle;
+        }
+    }
+}
