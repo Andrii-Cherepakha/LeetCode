5aa8b42b60060f68cf81f387d01280ab9472a43f
Andrii <Andrii@ANDRII-NOTEBOOK>
2019-11-10 20:28:30 +0100

    102 Binary Tree Level Order Traversal

---
+++ LeetCode/Trees/102_BinaryTreeLevelOrderTraversal.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Trees
+{
+    class BinaryTreeLevelOrderTraversal
+    {
+
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var topLevelQueue = new Queue<TreeNode>();
+            Queue<TreeNode> nextLevelQueue = null;
+            topLevelQueue.Enqueue(root);
+
+            while (nextLevelQueue == null || nextLevelQueue.Count > 0)
+            {
+                nextLevelQueue = new Queue<TreeNode>();
+                var intermidiate = new List<int>();
+
+                while (topLevelQueue.Count > 0)
+                {
+                    var node = topLevelQueue.Dequeue();
+                    intermidiate.Add(node.val);
+
+                    if (node.left != null) nextLevelQueue.Enqueue(node.left);
+                    if (node.right != null) nextLevelQueue.Enqueue(node.right);                    
+                }
+
+                result.Add(intermidiate);
+                topLevelQueue = nextLevelQueue;
+            }
+            
+            return result;
+        }
+    }
+}
