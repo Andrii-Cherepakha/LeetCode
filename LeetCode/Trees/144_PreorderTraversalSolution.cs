f15b759d645c10e1a45e4465dbff892fb8b02085
Andrii <Andrii@ANDRII-NOTEBOOK>
2019-11-10 16:24:44 +0100

    144 Preorder Traversal

---
+++ LeetCode/Trees/144_PreorderTraversalSolution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Trees
+{
+    class PreorderTraversalSolution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root == null) return result;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                result.Add(node.val);
+
+                if (node.right != null) stack.Push(node.right);
+                if (node.left != null) stack.Push(node.left);
+            }
+
+            return result;
+        }
+    }
+}
