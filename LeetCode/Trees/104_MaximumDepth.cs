e1c6f78bd8c61db95b57e6af9450ee68db1557ef
Andrii <Andrii@ANDRII-NOTEBOOK>
2019-11-11 19:35:15 +0100

    104 Maximum Depth

---
+++ LeetCode/Trees/104_MaximumDepth.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Trees
+{
+    class MaximumDepth
+    {
+        [Test]
+        public void MaximumDepthTest()
+        {
+            TreeNode root = new TreeNode(3);
+            root.left = new TreeNode(9);
+            root.right = new TreeNode(20);
+
+            root.left.right = new TreeNode(11);
+
+            root.left.right.left = new TreeNode(12);
+
+            root.right.left = new TreeNode(15);
+            root.right.right = new TreeNode(7);
+
+            Console.WriteLine(MaxDepth(root));
+        }
+        public int MaxDepthRecursion(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            return 1 + Math.Max(MaxDepthRecursion(root.left), MaxDepthRecursion(root.right));
+        }
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            int depth = 0;
+
+            while (queue.Count > 0)
+            {
+                depth++;
+                int cnt = queue.Count; // remember size, it is changed !!!
+                for (int i = 0; i < cnt; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+
+            return depth;
+        }
+    }
+}
