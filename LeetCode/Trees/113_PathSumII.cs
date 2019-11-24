c37a34bbf0c7d63b6291637cb524da7425e154a7
Andrii <Andrii@ANDRII-NOTEBOOK>
2019-11-24 14:27:42 +0100

    113 PathSumII

---
+++ LeetCode/Trees/113_PathSumII.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees
+{
+
+
+    class PathSumII
+    {
+
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            RecursionPreOrder(root, sum, new List<int>());
+            return result;
+        }
+
+        private List<IList<int>> result = new List<IList<int>>();
+
+        private void RecursionPreOrder(TreeNode node, int sum, List<int> path)
+        {
+            if (node == null) return;
+            
+            path.Add(node.val);
+
+            if (node.left == null && node.right == null && node.val == sum)
+            {
+                result.Add(new List<int>(path));
+            }
+
+            if (node.left != null)
+            {
+                RecursionPreOrder(node.left, sum - node.val, path);
+            }
+
+            if (node.right != null)
+            {
+                RecursionPreOrder(node.right, sum - node.val, path);
+            }
+
+            path.RemoveAt(path.Count - 1);        
+        }
+    }
+}
