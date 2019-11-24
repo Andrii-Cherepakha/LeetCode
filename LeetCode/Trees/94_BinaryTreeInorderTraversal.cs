1ef40b57261df08a02ba64d3d8dde193175678b7
Andrii <Andrii@ANDRII-NOTEBOOK>
2019-11-24 15:32:32 +0100

    94 BinaryTreeInorderTraversal

---
+++ LeetCode/Trees/94_BinaryTreeInorderTraversal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees
+{
+    class BinaryTreeInorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            InorderTraversalRecursion(root);
+            return result;
+        }
+
+        IList<int> result = new List<int>();
+
+        private void InorderTraversalRecursion(TreeNode node)
+        {
+            if (node == null) return;
+
+            if (node.left != null) InorderTraversalRecursion(node.left);
+            result.Add(node.val);
+            if (node.right != null) InorderTraversalRecursion(node.right);
+        }
+    }
+}
