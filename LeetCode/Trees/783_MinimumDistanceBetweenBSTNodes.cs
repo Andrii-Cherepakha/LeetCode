8446fad624600014df9c1df0c774b00c64037696
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-16 00:02:25 +0100

    MinimumDistanceBetweenBSTNodes

---
+++ LeetCode/Trees/783_MinimumDistanceBetweenBSTNodes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Trees
+{
+    class MinimumDistanceBetweenBSTNodes // == 530
+    {
+        public int MinDiffInBST(TreeNode root)
+        {
+            if (root == null) return min;
+
+            MinDiffInBST(root.left);
+
+            if (prev != null)
+            {
+                min = Math.Min(min, root.val - prev.val);
+            }
+
+            prev = root;
+
+            MinDiffInBST(root.right);
+
+            return min;
+        }
+
+        private int min = int.MaxValue;
+        private TreeNode prev;
+    }
+}
