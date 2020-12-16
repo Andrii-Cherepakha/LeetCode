d2752f0a86bb4d4e47eaf7cf8b629194f12d11b8
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-16 23:35:13 +0100

    _653_TwoSumBST

---
+++ LeetCode/Trees/653_TwoSumBST.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Trees
+{
+    public class _653_TwoSumBST
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            var current = root;
+            var stack = new Stack<TreeNode>();
+            var hash = new HashSet<int>();
+
+            while (current != null || stack.Count > 0)
+            {
+                while (current != null)
+                {
+                    stack.Push(current);
+                    current = current.left;
+                }
+
+                current = stack.Pop();
+                hash.Add(current.val);
+
+                current = current.right;
+            }
+
+            foreach (var val in hash)
+            {
+                int diff = k - val;
+                if (val != diff && hash.Contains(diff))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
