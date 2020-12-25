060e24c588b871e945db393073039c607e5d87e3
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-25 22:59:23 +0100

    _109_ConvertSortedListToBST

--- LeetCode/LinkedList/ListNode.cs
+++ LeetCode/LinkedList/ListNode.cs
@@ -9,5 +9,11 @@
         {
             val = x;
         }
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
     }
 }
---
+++ LeetCode/Trees/109_ConvertSortedListToBST.cs
@@ -0,0 +1,51 @@
+﻿using LeetCode.LinkedList;
+
+namespace LeetCode.Trees
+{
+    public class _109_ConvertSortedListToBST
+    {
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            if (head == null) return null;
+
+            current = head;
+
+            return InOrder(0, GetListLength(head) - 1);
+        }
+
+        private ListNode current;
+
+        private TreeNode InOrder(int start, int end)
+        {
+            if (start > end) return null;
+
+            int mid = start + (end - start + 1) / 2;
+
+            var left = InOrder(start, mid - 1);
+
+            var node = new TreeNode(current.val);
+            current = current.next;
+
+            var right = InOrder(mid + 1, end);
+
+            node.left = left;
+            node.right = right;
+
+            return node;
+        }
+
+        private int GetListLength(ListNode head)
+        {
+            int l = 0;
+
+            while (head != null)
+            {
+                l++;
+                head = head.next;
+            }
+
+            return l;
+        }
+
+    }
+}
