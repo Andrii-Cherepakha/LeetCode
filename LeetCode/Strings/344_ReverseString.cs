7fdb7d3ae5b1d27f0ecfbe2851a2a5ed04e46df7
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-29 23:34:00 +0100

    _344_ReverseString

---
+++ LeetCode/Strings/344_ReverseString.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode.Strings
+{
+    public class _344_ReverseString
+    {
+        [Test]
+        public void Test()
+        {
+            ReverseString(new[] {'h','e','l','l','o'});
+        }
+        
+        public void ReverseString(char[] s)
+        {
+            if (s == null || s.Length == 0 || s.Length == 1) return;
+
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+}
