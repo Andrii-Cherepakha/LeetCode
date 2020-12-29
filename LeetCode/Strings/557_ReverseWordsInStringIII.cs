3997576685804941bd91b81870268bd8f5fcf09f
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-29 23:59:44 +0100

    _557_ReverseWordsInStringIII

---
+++ LeetCode/Strings/557_ReverseWordsInStringIII.cs
@@ -0,0 +1,43 @@
+﻿
+namespace LeetCode.Strings
+{
+    public class _557_ReverseWordsInStringIII
+    {
+        public string ReverseWords(string s)
+        {
+            var str = s.ToCharArray();
+
+            int start = 0;
+            int end = -1;
+
+            for(int i=0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    ReversWord(str, start, end);
+                    start = i + 1;
+                    end = i;
+                }
+                else 
+                    end++;
+            }
+
+            ReversWord(str, start, end);
+
+            return new string(str);
+        }
+
+        private void ReversWord(char[] s, int start, int end)
+        {
+            while (start < end)
+            {
+                char tmp = s[start];
+                s[start] = s[end];
+                s[end] = tmp;
+
+                start++;
+                end--;
+            }
+        }
+    }
+}
