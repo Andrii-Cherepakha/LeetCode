8c5a6806e41ab4227cb3d86531f1ed01462883b5
Andrii <Andrii@ANDRII-NOTEBOOK>
2020-12-31 00:40:28 +0100

    _14_LongestCommonPrefix

---
+++ LeetCode/Strings/14_LongestCommonPrefix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Strings
+{
+    class _14_LongestCommonPrefix
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            if (strs == null || strs.Length == 0) return "";
+            
+            var result = new StringBuilder();
+            
+            int length = int.MaxValue;
+            foreach (var str in strs)
+            {
+                length = Math.Min(length, str.Length);
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                var ch = strs[0][i];
+                var theSame = true;
+                for (int j = 1; j < strs.Length; j++)
+                {
+                    theSame = theSame & ch == strs[j][i];
+                }
+
+                if (theSame) result.Append(ch); else break;
+            }
+
+            return result.ToString();
+        }
+    }
+}
