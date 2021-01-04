2bc95cbba559f5784c2bd028701407e3922c0e30
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-04 14:49:02 +0100

    _1672_RichestCustomerWealth

---
+++ LeetCode/Array2D/1672_RichestCustomerWealth.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetCode.Array2D
+{
+    class _1672_RichestCustomerWealth
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int max = int.MinValue;
+
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < accounts[i].Length; j++)
+                {
+                    sum += accounts[i][j];
+                }
+
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+    }
+}
