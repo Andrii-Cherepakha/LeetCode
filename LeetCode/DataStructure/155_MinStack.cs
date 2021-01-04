a33986e5fd849d3baa2788f1eb3a8d20b88eec86
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-04 14:35:51 +0100

    _509_FibonacciNumber

---
+++ LeetCode/DataStructure/155_MinStack.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.DataStructure
+{
+    class _155_MinStack
+    {
+        private class Storage
+        {
+            public int Value { get; set; }
+            public int Min { get; set; }
+        }
+        
+        private Stack<Storage> _stack = new Stack<Storage>();
+
+        //public MinStack()
+        //{
+
+        //}
+
+        public void Push(int x)
+        {
+            if (_stack.Count == 0)
+            {
+                _stack.Push(new Storage { Value = x, Min = x });
+            }
+            else
+            {
+                int prevMin = GetMin();
+                _stack.Push(new Storage { Value = x, Min = Math.Min(x, prevMin) });
+            }
+        }
+
+        public void Pop()
+        {
+            if (_stack.Count == 0) return;
+            _stack.Pop();
+        }
+
+        public int Top()
+        {
+            return _stack.Peek().Value;
+        }
+
+        public int GetMin()
+        {
+            return _stack.Peek().Min;
+        }
+    }
+}
---
+++ LeetCode/Mathematics/509_FibonacciNumber.cs
@@ -0,0 +1,25 @@
+﻿
+namespace LeetCode.Mathematics
+{
+    class _509_FibonacciNumber
+    {
+        public int Fib(int n)
+        {
+            if (n <= 1) return n;
+            if (n == 2) return 2;
+
+            int cur = 0;
+            int prev1 = 1;
+            int prev2 = 1;
+
+            for (int i = 3; i <=n; i++)
+            {
+                cur = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = cur;
+            }
+
+            return cur;
+        }
+    }
+}
