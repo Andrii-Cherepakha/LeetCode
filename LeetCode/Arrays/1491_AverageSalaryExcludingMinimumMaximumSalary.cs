830bf68b824b752603b28cf2c2da30afbfbb4ce0
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-04 21:08:41 +0100

    _1491_AverageSalaryExcludingMinimumMaximumSalary

---
+++ LeetCode/Arrays/1491_AverageSalaryExcludingMinimumMaximumSalary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LeetCode.Arrays
+{
+    class _1491_AverageSalaryExcludingMinimumMaximumSalary
+    {
+        public double Average(int[] salary)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            double sum = 0;
+
+            foreach (int value in salary)
+            {
+                sum += value;
+                max = Math.Max(value, max);
+                min = Math.Min(value, min);
+            }
+
+            sum = sum - max - min;
+            return sum / (salary.Length - 2);
+        }
+    }
+}
