717e8a0ec36b65c44d929c9a86c114149dbacdc6
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-06 22:21:24 +0100

    _3_LongestSubstringWithoutRepeatingCharacters

---
+++ LeetCode/Strings/3_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Strings
+{
+    class _3_LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+            
+            int p = 0;
+            int max = 0;
+            var hash = new HashSet<char>();
+            hash.Add(s[0]);
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (hash.Contains(s[i]))
+                {
+                    max = Math.Max(max, i - p);
+                    while (hash.Contains(s[i]))
+                    {
+                        hash.Remove(s[p]);
+                        p++;
+                    }
+                }
+                hash.Add(s[i]);
+            }
+
+            max = Math.Max(max, s.Length - p);
+
+            return max;
+        }
+
+
+    }
+}
