13af344884f936d845fd816f844e5fd1080cee3f
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-07 23:34:20 +0100

    _347_TopKFrequentElements

---
+++ LeetCode/Arrays/347_TopKFrequentElements.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Arrays
+{
+    class _347_TopKFrequentElements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            var result = new int[k];
+
+            if (nums == null || nums.Length == 0) return result;
+
+            var dict = new Dictionary<int, int>();
+
+            foreach (var num in nums) 
+            {
+                dict[num] = dict.ContainsKey(num) ? dict[num] + 1 : 1;
+            }
+             
+            while (k > 0 && dict.Count > 0)
+            {
+                int max = 0;
+                int key = 0;
+
+                foreach (var num in dict.Keys)
+                {
+                    if (dict[num] > max)
+                    {
+                        max = dict[num];
+                        key = num;
+                    }
+                }
+
+                result[result.Length - k] = key;
+                dict.Remove(key);
+
+                k--;
+            }
+
+            return result;
+        }
+    }
+}
