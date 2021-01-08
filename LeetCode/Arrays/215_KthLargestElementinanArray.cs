1da875b736823712e6b8f493e406ca1f0db214c4
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-08 20:53:11 +0100

    _215_KthLargestElementinanArray

---
+++ LeetCode/Arrays/215_KthLargestElementinanArray.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LeetCode.Arrays
+{
+    class _215_KthLargestElementinanArray
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            Array.Sort(nums);
+            return nums[nums.Length - k];
+        }
+    }
+}
