18b2c4d6c9e7f040a5fdbf222988dfbd76a0ba4a
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-10 22:54:51 +0100

    _171_ExcelSheetColumnNumber

---
+++ LeetCode/Strings/171_ExcelSheetColumnNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Strings
+{
+    class _171_ExcelSheetColumnNumber
+    {
+        public int TitleToNumber(string s)
+        {
+            // A = 65 Z = 90
+
+            int res = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                res = res * 26 + (s[i] - 64);
+            }
+
+            return res;
+        }
+    }
+}
