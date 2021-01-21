af94048727a530e4ebd2509786a70255be82db1e
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-01-22 00:23:38 +0100

    _647_PalindromicSubstrings

---
+++ LeetCode/Strings/647_PalindromicSubstrings.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace LeetCode.Strings
+{
+    class _647_PalindromicSubstrings
+    {
+        public int CountSubstrings(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return 0;
+
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                result += GetNumberOfPalindroms(s, i, i);
+                result += GetNumberOfPalindroms(s, i, i+1);
+            }
+
+            return result;
+        }
+
+        private int GetNumberOfPalindroms(string s, int i, int j)
+        {
+            int cnt = 0;
+
+            while (i >=0 && j < s.Length)
+            {
+                if (s[i] == s[j])
+                {
+                    cnt++;
+                }
+                else break;
+                i--;
+                j++;
+            }
+
+            return cnt;
+        }
+
+    }
+}
