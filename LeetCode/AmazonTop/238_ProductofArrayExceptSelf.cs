130e942096baf844a1b58e7f66774284f8d195fb
Andrii <Andrii@ANDRII-NOTEBOOK>
2021-02-23 20:47:58 +0100

    Amazone top

---
+++ LeetCode/AmazonTop/238_ProductofArrayExceptSelf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.AmazonTop
+{
+    class _238_ProductofArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+
+            if (nums == null) return null;
+            int[] res = new int[nums.Length];
+
+            int product = 1;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                product *= nums[i]; // product so far from left to right
+                res[i] = product;
+            }
+
+            product = 1;
+            for (int i = nums.Length - 1; i > 0; i--)
+            {
+                res[i] = res[i - 1] * product;
+                product *= nums[i]; // product so far from right to left
+            }
+
+            res[0] = product;
+
+            return res;
+        }
+    }
+}
---
+++ LeetCode/AmazonTop/380_InsertDeleteGetRandom.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.AmazonTop
+{
+    class _380_InsertDeleteGetRandom
+    {
+        /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+        public bool Insert(int val)
+        {
+            if (dict.ContainsKey(val)) return false;
+
+            list.Add(val);
+            dict.Add(val, list.Count - 1);
+            return true;
+        }
+
+        /** Removes a value from the set. Returns true if the set contained the specified element. */
+        public bool Remove(int val)
+        {
+            if (!dict.ContainsKey(val)) return false;
+
+            int lastKey = list.ElementAt(list.Count - 1);
+            dict[lastKey] = dict[val];
+            list[dict[lastKey]] = lastKey;
+
+            list.RemoveAt(list.Count - 1);
+            dict.Remove(val);
+            return true;
+        }
+
+        /** Get a random element from the set. */
+        public int GetRandom()
+        {
+            int index = rand.Next(0, list.Count);
+            return list.ElementAt(index);
+        }
+
+        private List<int> list = new List<int>();
+        private Dictionary<int, int> dict = new Dictionary<int, int>(); // key, index
+        private Random rand = new Random();
+    }
+}
